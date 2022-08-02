12ce5ab8a386af87cc76b11102ff60349266b761
Felipe Hlibco <felipehlibco@google.com>
2022-08-01 19:16:26 -0700

    Add dialogflow integration snippets
    Change-Id: I0c53fdffb931a089d9e8395869627d45c0c498be

---
+++ create-knowledgebase-document-csv.py
@@ -0,0 +1,64 @@
+## Copyright 2022 Google LLC
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     https://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+"""This code creates a knowledge base document from a CSV document and adds it to the knowledge base.
+
+Read more: https://developers.google.com/business-communications/business-messages/guides/how-to/integrate/dialogflow?method=api#create-document
+
+This code is based on the https://github.com/google-business-communications/python-businessmessages
+Python Business Messages client library.
+"""
+
+from oauth2client.service_account import ServiceAccountCredentials
+from businesscommunications.businesscommunications_v1_client import BusinesscommunicationsV1
+from businesscommunications.businesscommunications_v1_messages import (
+    BusinesscommunicationsBrandsAgentsIntegrationsPatchRequest,
+    DialogflowEsIntegration,
+    DialogflowKnowledgebase,
+    DialogflowDocument
+)
+
+# Edit the values below:
+BRAND_ID = 'EDIT_HERE'
+AGENT_ID = 'EDIT_HERE'
+INTEGRATION_ID = 'EDIT_HERE'
+SCOPES = ['https://www.googleapis.com/auth/businesscommunications']
+SERVICE_ACCOUNT_FILE = './service_account_key.json'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+client = BusinesscommunicationsV1(credentials=credentials)
+
+integrations_service = BusinesscommunicationsV1.BrandsAgentsIntegrationsService(client)
+
+integration_name = 'brands/' + BRAND_ID + '/agents/' + AGENT_ID + '/integrations/' + INTEGRATION_ID
+
+update_request = integrations_service.Patch(BusinesscommunicationsBrandsAgentsIntegrationsPatchRequest(
+  integration=DialogflowEsIntegration(
+    dialogflowKnowledgeBases=[DialogflowKnowledgebase(
+      displayName='My knowledge base display name',
+      documents=[
+        DialogflowDocument(
+          displayName='Document display name',
+          rawContent='CSV raw bytes'
+        )
+      ]
+    )]
+  ),
+  name=integration_name,
+  updateMask='dialogflowEsIntegration.dialogflowKnowledgeBases'
+))
+
+print(update_request)
---
+++ get-partner.py
@@ -0,0 +1,48 @@
+## Copyright 2022 Google LLC
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     https://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+"""This code gets a partner.
+
+Read more: https://developers.google.com/business-communications/business-messages/reference/business-communications/rest/v1/partners/get
+
+This code is based on the https://github.com/google-business-communications/python-businessmessages
+Python Business Messages client library.
+"""
+
+from oauth2client.service_account import ServiceAccountCredentials
+from businesscommunications.businesscommunications_v1_client import BusinesscommunicationsV1
+from businesscommunications.businesscommunications_v1_messages import (
+    Agent,
+    BusinesscommunicationsPartnersGetRequest,
+)
+
+# Edit the values below:
+PARTNER_ID = 'EDIT_HERE'
+SCOPES = ['https://www.googleapis.com/auth/businesscommunications']
+SERVICE_ACCOUNT_FILE = './service_account_key.json'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+client = BusinesscommunicationsV1(credentials=credentials)
+
+partners_service = BusinesscommunicationsV1.PartnersService(client)
+
+partner_name = 'partners/' + PARTNER_ID
+
+partner = partners_service.Get(BusinesscommunicationsPartnersGetRequest(
+        name=partner_name
+    ))
+
+print(partner)
